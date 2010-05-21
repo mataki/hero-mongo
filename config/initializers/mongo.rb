2ea977273c784bdeccf2866fb8444d4cb3269e26
mat_aki <matsumura.aki@gmail.com>
2010-05-21 20:40:05 +0900

    Delete setting

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,9 +1,5 @@
 require "mongo_mapper"
 
-if ENV['MONGOHQ_URL']
-  MongoMapper.config = MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
-else
-  MongoMapper.config = {RAILS_ENV => {'uri' => "mongodb://#{ENV["MONGOHQ_USER"]}:#{ENV["MONGOHQ_PASS"]}@flame.mongohq.com:27033/matlocaltest"}}
-end
+MongoMapper.config = MongoMapper.config = {RAILS_ENV => {'uri' => ENV['MONGOHQ_URL']}}
 
 MongoMapper.connect(RAILS_ENV)
